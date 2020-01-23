d069cc9f6d8796964a7eba5c0ce10bc1bc23ea25
Jared Fleming <jaredmf10@gmail.com>
2020-01-23 16:10:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,3 +52,12 @@ def reduce_to_total(source_array, starting_point)
     end
     return new
   end
+
+  def reduce_to_any_true(source_array)
+    i = 0
+    while i < source_array.length do
+      return true if source_array[i]
+      i += 1
+    end
+    return false
+  end
