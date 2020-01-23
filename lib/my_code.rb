6fa75cd19e3d30f4582e649b8a5e4807b71f078d
Jared Fleming <jaredmf10@gmail.com>
2020-01-23 16:11:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -43,7 +43,7 @@ def map_to_square(source_array)
   squared_array
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point= 0)
     new = 0 + starting_point
     i = 0
     while i < source_array.length do
